93626726e643857059fa29f3e5cec742ca3232b4
Liu Wei <wliu272@163.com>
2014-04-02 22:51:19 +0800

    refactored jquery validate code

--- demo-app/assets/linker/js/custom.validate.js
+++ demo-app/assets/linker/js/custom.validate.js
@@ -19,359 +19,115 @@ $(document).ready(function () {
     return this.optional(element) || ajax_validaterefundbalance(value);
   }, "您确定到账了这么多钱");
 
-	$("#haha-form-paypass-update").validate({
-    debug: true,
-    rules: {
+
+	jquery_validate(
+		"#haha-form-paypass-update", {
 			old_paypass: { required: true, validPaypass: true },
       paypass: { required: true },
       paypass_confirm: { equalTo: "#paypass" },
-    },
-    messages: {
+    }, {
 			old_paypass: { required: "请输入旧支付密码", validPaypass: "旧支付密码输入错误" },
       paypass: { required: "请输入密码" },
       paypass_confirm: { equalTo: "两次输入的密码不匹配" },
-    },
-    highlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-success');
-      fg.addClass('has-error');
-    },
-    unhighlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-error');
-      $(element).closest('.validate-message').remove();
-    },
-    success: function(element) {
-      fg = $(element).closest('.form-group');
-			fg.removeClass('has-error');
-			fg.addClass('has-success');
-      $(element).closest('.validate-message').remove();
-		},
-		errorElement: 'span',
-		errorClass: 'haha-validate-error validate-message',
-		/*
-		errorPlacement: function(error, element) {
-			error.insertAfter($(element).closest(".input-group"));
-		},
-		*/
-    submitHandler: function(form) {
-      form.submit();
     }
-  });
+	);
 
-  $("#haha-form-withdraw").validate({
-    debug: true,
-    rules: {
+	jquery_validate(
+		"#haha-form-withdraw", {
       withdraw_amount: { required: true, digits: true, validBalance: true },
       withdraw_amount_confirm: { equalTo: "#withdraw_amount" },
 			paypass: { required: true, validPaypass: true }
-    },
-    messages: {
+    }, {
       withdraw_amount: { required: "请输入提现金额", digits: "您的输入不是数字", validBalance: "钱不够啦" },
       withdraw_amount_confirm: { equalTo: "两次输入的提现金额不匹配" },
 			paypass: { required: "请输入支付密码", validPaypass: "支付密码输入错误" }
-    },
-    highlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-success');
-      fg.addClass('has-error');
-    },
-    unhighlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-error');
-      $(element).closest('.validate-message').remove();
-    },
-    success: function(element) {
-      fg = $(element).closest('.form-group');
-			fg.removeClass('has-error');
-			fg.addClass('has-success');
-      $(element).closest('.validate-message').remove();
-		},
-		errorElement: 'span',
-		errorClass: 'haha-validate-error validate-message',
-		errorPlacement: function(error, element) {
-			if ($(element).parent().attr("class") == "input-group") {
-				error.insertAfter($(element).closest(".input-group"));
-			} else {
-				error.insertAfter($(element));
-			}
-		},
-    submitHandler: function(form) {
-      form.submit();
     }
-  });
+	);
 
-	$("#haha-form-refundplatform").validate({
-    debug: true,
-    rules: {
+	jquery_validate(
+		"#haha-form-refundplatform", {
       refundplatform_amount: { required: true, digits: true, validRefundBalance: true },
       refundplatform_amount_confirm: { equalTo: "#refundplatform_amount" },
-    },
-    messages: {
+    }, {
       refundplatform_amount: { required: "请输入还款金额", digits: "您的输入不是数字", validRefundBalance: "您不需要还这么多" },
       refundplatform_amount_confirm: { equalTo: "两次输入的金额不匹配" },
-    },
-    highlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-success');
-      fg.addClass('has-error');
-    },
-    unhighlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-error');
-      $(element).closest('.validate-message').remove();
-    },
-    success: function(element) {
-      fg = $(element).closest('.form-group');
-			fg.removeClass('has-error');
-			fg.addClass('has-success');
-      $(element).closest('.validate-message').remove();
-		},
-		errorElement: 'span',
-		errorClass: 'haha-validate-error validate-message',
-		errorPlacement: function(error, element) {
-			error.insertAfter($(element).closest(".input-group"));
-		},
-    submitHandler: function(form) {
-      form.submit();
     }
-  });
+	);
 
-	$("#haha-form-refundinvestor").validate({
-    debug: true,
-    rules: {
+	jquery_validate(
+		"#haha-form-refundinvestor", {
       refundinvestor_amount: { required: true, digits: true },
       refundinvestor_amount_confirm: { equalTo: "#refundinvestor_amount" },
-    },
-    messages: {
+    }, {
       refundinvestor_amount: { required: "请输入还款金额", digits: "您的输入不是数字" },
       refundinvestor_amount_confirm: { equalTo: "两次输入的金额不匹配" },
-    },
-    highlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-success');
-      fg.addClass('has-error');
-    },
-    unhighlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-error');
-      $(element).closest('.validate-message').remove();
-    },
-    success: function(element) {
-      fg = $(element).closest('.form-group');
-			fg.removeClass('has-error');
-			fg.addClass('has-success');
-      $(element).closest('.validate-message').remove();
-		},
-		errorElement: 'span',
-		errorClass: 'haha-validate-error validate-message',
-		errorPlacement: function(error, element) {
-			error.insertAfter($(element).closest(".input-group"));
-		},
-    submitHandler: function(form) {
-      form.submit();
     }
-  });
-	
-  $("#haha-form-recharge").validate({
-    debug: true,
-    rules: {
+	);
+
+	jquery_validate(
+		"#haha-form-recharge", {
       recharge_amount: { required: true, digits: true },
       recharge_amount_confirm: { equalTo: "#recharge_amount" },
-    },
-    messages: {
+    }, {
       recharge_amount: { required: "请输入充值金额", digits: "您的输入不是数字" },
       recharge_amount_confirm: { equalTo: "两次输入的充值金额不匹配" },
-    },
-    highlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-success');
-      fg.addClass('has-error');
-    },
-    unhighlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-error');
-      $(element).closest('.validate-message').remove();
-    },
-    success: function(element) {
-      fg = $(element).closest('.form-group');
-			fg.removeClass('has-error');
-			fg.addClass('has-success');
-      $(element).closest('.validate-message').remove();
-		},
-		errorElement: 'span',
-		errorClass: 'haha-validate-error validate-message',
-		errorPlacement: function(error, element) {
-			error.insertAfter($(element).closest(".input-group"));
-		},
-    submitHandler: function(form) {
-      form.submit();
     }
-  });
-  $("#haha-form-invest").validate({
-    debug: true,
-    rules: {
+	);
+
+	jquery_validate(
+		"#haha-form-invest", {
       invest_amount: { required: true, digits: true },
       invest_amount_confirm: { equalTo: "#invest_amount" },
 			paypass: { required: true, validPaypass: true },
       invest_contract_agreed: { required: true }
-    },
-    messages: {
+    }, {
       invest_amount: { required: "请输入投资金额", digits: "您的输入不是数字" },
       invest_amount_confirm: { equalTo: "两次输入的投资金额不匹配" },
 			paypass: { required: "请输入支付密码", validPaypass: "支付密码输入错误" },
       invest_contract_agreed: { required: "您还没有同意投资合同" }
-    },
-    highlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-success');
-      fg.addClass('has-error');
-    },
-    unhighlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-error');
-      $(element).closest('.validate-message').remove();
-    },
-    success: function(element) {
-      fg = $(element).closest('.form-group');
-			fg.removeClass('has-error');
-			fg.addClass('has-success');
-      $(element).closest('.validate-message').remove();
-		},
-		errorElement: 'span',
-		errorClass: 'haha-validate-error validate-message',
-		errorPlacement: function(error, element) {
-			if ($(element).parent().attr("class") == "input-group") {
-				error.insertAfter($(element).closest(".input-group"));
-			}	else if ($(element).next("div").attr("class") == "bootstrap-select") {
-				error.insertAfter($(element).next("div#bootstrap-select"));
-			} else {
-				error.insertAfter($(element));
-			}
-		},
-    submitHandler: function(form) {
-      form.submit();
     }
-  });
+	);
 
-	$('#haha-form-signup').validate({
-    debug: true,
-		rules: {
-			user_name: {
-				required: true
-			},
-			name: {
-				required: true
-			},
-			email: {
-				required: true,
-				email: true
-			},
-			password: {
-				minlength: 6,
-				required: true
-			},
-			confirmation: {
-				equalTo: "#password"
-			},
-      id_card: {
-        required: true,
-        validIdCard: true
-      },
-      captcha: {
-        required: true,
-        validCaptcha: true
-      }
-		},
-    messages: {
+	jquery_validate(
+		'#haha-form-signup', {
+			user_name: { required: true },
+			name: { required: true },
+			email: { required: true, email: true },
+			password: { minlength: 6, required: true },
+			confirmation: { equalTo: "#password" },
+      id_card: { required: true, validIdCard: true },
+      captcha: { required: true, validCaptcha: true }
+		}, {
       user_name: { required: "请输入用户名" },
       name: { required: "请输入您的真实姓名" },
       email: { required: "请输入你的常用邮箱地址", email: "电子邮箱格式不正确" },
       password: { minlength: "密码至少应为{0}位", required: "请输入密码" },
       confirmation: { equalTo: "两次输入的密码不匹配" },
       id_card: { required: "请输入身份证号码" }
-    },
-    highlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-success');
-      fg.addClass('has-error');
-    },
-    unhighlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-error');
-      $(element).closest('.validate-message').remove();
-    },
-    success: function(element) {
-      fg = $(element).closest('.form-group');
-			fg.removeClass('has-error');
-			fg.addClass('has-success');
-      $(element).closest('.validate-message').remove();
-		},
-		errorElement: 'span',
-		errorClass: 'haha-validate-error validate-message',
-		//errorClass: 'help-block validate-message',
-    //focusCleanup: true,
-		errorPlacement: function(error, element) {
-			error.insertAfter(element);
-		}
-	});
-
-	$('#haha-form-signin').validate({
-    debug: true,
-		rules: {
-      captcha: {
-        required: true,
-        validCaptcha: true
-      }
-		},
-    messages: {
-      captcha: { required: "请输入验证码", validCaptcha: '验证码输入错误' }
-    },
-    highlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-success');
-      fg.addClass('has-error');
-    },
-    unhighlight: function(element) {
-      fg = $(element).closest('.form-group');
-      fg.removeClass('has-error');
-      $(element).closest('.validate-message').remove();
-    },
-    success: function(element) {
-      fg = $(element).closest('.form-group');
-			fg.removeClass('has-error');
-			fg.addClass('has-success');
-      $(element).closest('.validate-message').remove();
-		},
-		errorElement: 'span',
-		errorClass: 'haha-validate-error validate-message',
-		//errorClass: 'help-block validate-message',
-    //focusCleanup: true,
-		errorPlacement: function(error, element) {
-			if ($(element).parent().attr("class") == "input-group") {
-				error.insertAfter($(element).closest(".input-group"));
-			} else {
-				error.insertAfter($(element));
-			}
-		},
-    submitHandler: function(form) {
-      form.submit();
     }
-	});
+	);
+	
+	jquery_validate(
+		'#haha-form-signin', {
+      captcha: { required: true, validCaptcha: true }
+		}, {
+      captcha: { required: "请输入验证码", validCaptcha: '验证码输入错误' }
+    }
+	);
 
-	var rules = {
-		bank_province: { required: true },
-		bank_city: { required: true },
-		bank_name: { required: true },
-		bank_account: { required: true }
-	};
-	var messages = {
-		bank_province: { required: "您必须选择开户行所在省份" },
-		bank_city: { required: "您必须选择开户行所在城市" },
-		bank_name: { required: "您必须提供开户行的全称" },
-		bank_account: { required: "您必须提供您的银行帐号" }
-	};
-	jquery_validate('#bindbank-form', rules, messages);
+	jquery_validate(
+		'#bindbank-form', {
+			bank_province: { required: true },
+			bank_city: { required: true },
+			bank_name: { required: true },
+			bank_account: { required: true }
+		}, {
+			bank_province: { required: "您必须选择开户行所在省份" },
+			bank_city: { required: "您必须选择开户行所在城市" },
+			bank_name: { required: "您必须提供开户行的全称" },
+			bank_account: { required: "您必须提供您的银行帐号" }
+		}
+	);
 	
 });
 
